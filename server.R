6de66513658a68d2dd52d4128d19bc9d0beede1d
James Maino <jamesmaino@gmail.com>
2017-05-11 19:33:48 +1000

    Animation speed Binsize

--- server.R
+++ server.R
@@ -20,22 +20,16 @@ shinyServer(function(input, output, session) {
   zipdata<- reactive({
     if(input$species=='punctigera'){
       return(subset(cleantable,
-                    as.numeric(format(as.Date(yearweek), '%Y')) == input$myYear))  
+                    as.Date(yearweek) >= input$dateMin&as.Date(yearweek) <= input$dateMax))  
     }else{
       return(subset(cleantable1,
-                    as.numeric(format(as.Date(yearweek), '%Y')) == input$myYear))
+                    as.Date(yearweek) >= input$dateMin&as.Date(yearweek) <= input$dateMax))
     }
   })
   
   ## Interactive Map ###########################################
   output$yearSlider <- renderUI({
-    subct <- subset(zipdata(), format(zipdata()$yearweek, '%Y') == input$myYear)
-    if(is.null(input$date)){
-      myDates <- c(as.Date(paste0(input$myYear,'-08-20')),as.Date(paste0(input$myYear,'-08-27')))
-      }else{
-        myDates<-input$date
-    }
-    sliderInput('date', 'Map showing trap data for date range:', min=min(as.Date(subct$yearweek)), max=max(as.Date(subct$yearweek)), value = myDates, width = '100%', step = 1)
+    sliderInput('date', 'Map showing trap data for date range:', min=input$dateMin, max=input$dateMax, value = c(input$dateMin, input$dateMin+input$binSize*7), width = '100%', step = input$binSize*7,animate = animationOptions(interval = input$aniSpeed*1000/as.numeric((input$dateMax-input$dateMin)/(input$binSize*7)),loop=TRUE))
    
   })
   
@@ -54,6 +48,7 @@ shinyServer(function(input, output, session) {
   weekdata<- reactive({
     start_date <- input$date[1]
     end_date <- input$date[2]
+    swd<-subset(zipdata(), yearweek >=start_date&yearweek<=end_date)
     swd%>%group_by_('id','latitude','longitude',"operator", "state", "district")%>%summarise(count=sum(count, na.rm = TRUE))
     })
   
@@ -72,7 +67,7 @@ shinyServer(function(input, output, session) {
     if (nrow(weekdata())==0){
       leafletProxy("map", data = weekdata()) %>%
         clearShapes() %>% clearMarkers() %>%
-        addMarkers(missing$longitude, missing$latitude, popup = 'No data at selected week',icon = myIcon)
+        addMarkers(missing$longitude, missing$latitude, icon = myIcon)
     }else{
       colorData <- weekdata()[[colorBy]]
       pal <- colorNumeric('YlOrRd', colorData)
@@ -83,105 +78,12 @@ shinyServer(function(input, output, session) {
                    stroke = TRUE, color = "black", weight = 1,
                    fillOpacity = ifelse(weekdata()[[sizeBy]]==0,0,0.8),
                    fillColor=pal(colorData)) %>%
-        addMarkers(missing$longitude, missing$latitude, popup = 'No data for selected date range',icon =  myIcon) %>%
+        addMarkers(missing$longitude, missing$latitude,icon =  myIcon) %>%
         addLegend("bottomleft", pal=pal, values=colorData, title='Count',
           layerId="colorLegend",opacity = 1)
       
     }
   })
 
-  output$timeMoths <- renderPlot({
-    # blank plot
-    p<-ggplot(data=data.frame())
-    mytheme <- theme_bw()+theme(text = element_text(size=rel(4)),
-                     axis.text.x = element_text(size=rel(5),angle = 45, hjust = 1),
-                     plot.title = element_text(size = 12, colour = "red"))
-    # build trap data
-    if(is.null(input$map_shape_click$id)){
-      noMapClick = TRUE 
-    }else{
-      noMapClick = !input$map_shape_click$id %in% unique(zipdata()$id)
-    }
-    if(noMapClick){
-      p = p + ggtitle('Click trap to overlay data')
-      trap_dat = NULL
-    }else{
-      trap_dat = subset(zipdata(), id == input$map_shape_click$id)
-      trap_dat$yearweek<-format(trap_dat$yearweek, format = '%Y-w%U')
-      if(nrow(trap_dat)>0){
-        trap_dat$yearweek<-as.Date(paste(trap_dat$yearweek,7),"%Y-w%U %u") # need to add day
-        trap_dat<-trap_dat%>%group_by_('yearweek')%>%summarise(count=sum(count, na.rm = TRUE))
-        p = p + geom_bar(data=trap_dat, aes(x=yearweek, y=count), alpha = 0.5, stat="identity",fill = '#F8766D', show.legend = TRUE, position=position_dodge())
-      }
-    }
-    
-    # set up state wide summary
-    if(input$region == 'all states'){
-      sdf<-zipdata()
-    }else{
-      sdf<-subset(zipdata(),state == input$region)
-    }
-    sdf$yearweek<-format(sdf$yearweek, format = '%Y-w%U')
-    if(nrow(sdf)>0)sdf$yearweek<-as.Date(paste(sdf$yearweek,7),"%Y-w%U %u") # need to add day
-    trapNo = length(unique(sdf$id))
-    state_dat<-sdf%>%group_by_('yearweek')%>%
-      summarise(se=sd(count)/sqrt(sum(!is.na(count))),count=mean(count, na.rm = TRUE))
-    state_dat$lwr <- state_dat$count - state_dat$se
-    state_dat$upr <- state_dat$count + state_dat$se
-    if(nrow(state_dat)>0){
-      p = p +
-        geom_line(data=state_dat, aes(x=yearweek, y=count), show.legend = TRUE) +
-        geom_ribbon(data=state_dat,aes(x = yearweek, ymin=lwr,ymax=upr),alpha = 0.3) +
-        xlab('') +
-        ylab(paste0('Mean weekly count of \n',trapNo,' traps in ',input$region, " (",format(mean(sdf$yearweek), format = '%Y'),")")) 
-      #+   scale_x_date(limits = c(min(sdf$yearweek)-7, max(sdf$yearweek)+7), date_breaks = "2 week",date_labels = "%b %d")
-    }else{p = p + ggtitle('No data for selected year or region')}
-    
-    return(p+mytheme)
-  })
-  
-  # output$timeMoths <- renderPlot({
-  #   p()# p() + geom_vline(xintercept=as.numeric(as.Date(input$date)),colour="black", linetype = "longdash", alpha = 0.5)
-  # })
-  # 
-  # Show a popup at the given location
-  showZipcodePopup <- function(id, lat, lng) {
-    selectedZip <- weekdata()[weekdata()$id == id,]
-    
-    content <- as.character(tagList(
-      # tags$h4("Count:", as.integer(selectedZip$count)),
-      tags$strong(HTML(sprintf("%s, %s",
-        selectedZip$district, selectedZip$state
-      ))), tags$br(),
-      sprintf("Trap operator: %s", selectedZip$operator),tags$br(),
-      sprintf("Count: %d", as.integer(selectedZip$count)),tags$br()
-    ))
-    if(length(selectedZip$count)==0){
-      content <-  as.character(tagList(
-        tags$h4("No trap data for selected date range")))
-    }
-    leafletProxy("map") %>% addPopups(lng, lat, content, layerId = id)
-  }
-
-  # When map is clicked, show a popup with city info
-  observe({
-    input$map_shape_click$id
-    leafletProxy("map") %>% clearPopups()
-    
-    if (is.null(input$map_shape_click$id))
-      return()
-    
-    isolate({
-      showZipcodePopup(input$map_shape_click$id, input$map_shape_click$lat, input$map_shape_click$lng)
-      })
-    
-  })
-  
-  observe({
-    input$date
-    leafletProxy("map") %>% clearPopups()
-  })
-  
-  
 
 })
--- ui.R
+++ ui.R
@@ -19,29 +19,18 @@ vars1 <- c(
 shinyUI(navbarPage(div(img(src="cesar_logo.png", width = 30, height = 30), "MothTrapVis"), id="nav", windowTitle = "MothTrapVis",
   tabPanel("Animation map",
     div(class="outer",
-
-      tags$head(
-        # Include our custom CSS
-        includeCSS("styles.css"),
-        includeScript("gomap.js")
-      ),
+      includeCSS("styles.css"),
       leafletOutput("map", width="100%", height="100%"),
-      tags$style(type='text/css', ".info legend leaflet-control {  }"), 
-      tags$style(type='text/css', ".irs-grid-text { font-size: 10pt; 
-                                                    color:rgba(255,255,255,0);}"), 
-      tags$style(type='text/css', ".irs-min { font-size: 14pt; }"),
-      tags$style(type='text/css', ".irs-max { font-size: 14pt; }"),
-      tags$style(type='text/css', ".irs-single { font-size: 14pt; }"), 
-      tags$style(type='text/css', ".irs-from { font-size: 14pt; }"), 
-      tags$style(type='text/css', ".irs-to { font-size: 14pt; }"), 
-      tags$style(type='text/css', "label.control-label { font-size: 14pt; }"),
       fixedPanel(id = 'selections',
                  top = '10%', draggable = FALSE, left = '5%', right = '5%',
                  bottom = "auto", width = 'auto', height = "auto",
                  fluidRow(
-                   column(1, selectInput( 'myYear','Year', selected = '2016', choices = unique(format(as.Date(cleantable$yearweek),'%Y')))),
                    column(2, selectInput("species", "Species", selected = 'punctigera', vars1)),
-                   column(2, selectInput("region", "Region for graph", selected = 'all states', vars)),
+                   
+                   column(1,dateInput("dateMin", "Start date",as.Date('2016-01-01'))),
+                   column(1,dateInput("dateMax", "End date",as.Date('2016-12-31'))),
+                   column(1,numericInput("binSize", "bin size (weeks)",2)),
+                   column(1,numericInput("aniSpeed", "Animation speed (s)",10)),
                                 img(src="logo.png", height = 70),
                                 img(src="SARDI_small.png", height = 70),
                                 img(src="DAFWA_small.png",height = 70),
@@ -54,43 +43,13 @@ shinyUI(navbarPage(div(img(src="cesar_logo.png", width = 30, height = 30), "Moth
                  top = "auto", width = 'auto', height = "auto",
                  column(uiOutput('yearSlider'),width = 10, offset = 1)),
 
-      absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-        draggable = TRUE, top = '30%', left = "auto", right = 20, bottom = "auto",
-        width = 330, height = "auto",
-        br(),
-        tags$head(tags$style(
-          HTML('
-               #sliderPanel {background-color: rgba(255,255,255,0);
-                             outline: none;
-                             border: 0;
-               #} 
-               #yearSlider {outline: none}')
-          )),
-        plotOutput("timeMoths", height = 300),
-        helpText("red bar graph shows data at selected trap\n
-                  grey bars show standard error"),
-        helpText(   a("Click Here for raw data", 
-                      href="https://docs.google.com/spreadsheets/d/16tksOn7SAv4ezJCX_z4no4MpJDtua6pGqVPjfg57OZ8/edit#gid=0", target="_blank"  )
-          )
-      ),
+      
       
       tags$div(id="cite",'cesar'
       )
     )
   ),
-  # tabPanel("About"),
-  
-  # tabPanel("Regional Summary",
-  #          fluidRow(br(),br(),
-  #            column(6,
-  #                   plotOutput("plotMothNumber")
-  #            ), h5("Trap locations used in plot shown below"),
-  #            column(5,
-  #                   leafletOutput("map1")
-  #            )
-  #          ),
-  #          hr()
-  # ),
+
 
   conditionalPanel("false", icon("crosshair"))
 ))
